c165f4874ce182ec4c5f194d6d180d1e79968bdc
Ataba <ahmadata178998@gmail.com>
2026-07-22 09:48:07 +0300

    Solved LIFO

---
+++ Easy/225.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return not self.q
+
+
+# Input
+# ["MyStack", "push", "push", "top", "pop", "empty"]
+# [[], [1], [2], [], [], []]
+# Output
+# [null, null, null, 2, 2, false]
+
+# 1 -> 2 root
+# 2 -> 1 tail
+
+# Explanation
+# MyStack myStack = new MyStack();
+# myStack.push(1);
+# myStack.push(2);
+# myStack.top(); // return 2
+# myStack.pop(); // return 2
+# myStack.empty(); // return False
