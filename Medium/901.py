bc78dd333ac94b0d1218b67de6288a3417f31be8
Ataba <ahmadata178998@gmail.com>
2026-07-22 23:38:43 +0300

    Solved neetcode Stack problem

---
+++ Medium/901.py
@@ -0,0 +1,35 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.s = []
+
+    def next(self, price: int) -> int:
+        span = 1
+
+        while self.s and self.s[-1][0] <= price:
+            _, prev_span = self.s.pop()
+            span += prev_span
+
+        self.s.append((price, span))
+        return span
+
+
+        
+        
+
+
+# Input
+# ["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+# [[], [100], [80], [60], [70], [60], [75], [85]]
+# Output
+# [null, 1, 1, 1, 2, 1, 4, 6]
+
+# Explanation
+# StockSpanner stockSpanner = new StockSpanner();
+# stockSpanner.next(100); // return 1
+# stockSpanner.next(80);  // return 1
+# stockSpanner.next(60);  // return 1
+# stockSpanner.next(70);  // return 2
+# stockSpanner.next(60);  // return 1
+# stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
+# stockSpanner.next(85);  // return 6
